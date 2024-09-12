e000be07bff777060df270f3856267c4d397a783
may <maylynnespinola@gmail.com>
2024-09-12 11:08:06 -0500

    accessed on

--- DS1/LB1toLB2/src/AccessedOn.java
+++ DS1/LB1toLB2/src/AccessedOn.java
@@ -1,7 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class AccessedOn {
-    if (new void File("AccessedOn.txt").exists()) {
+    public static void main(String[] args) throws IOException {
         File file = new File("AccessedOn.txt");
+        Date date = new Date();
+
+        if (!file.exists()){
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(date.toString());
+            writer.close();
+        }else{
+            FileWriter writer = new FileWriter(file, true);
+            writer.write("\n" + date);
+            writer.close();
+        }
+
+
     }
 }
+
