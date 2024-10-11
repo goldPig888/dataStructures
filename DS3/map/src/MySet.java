b48ba2430a04d83efd5d91af1c3370195ae5ed47
may <maylynnespinola@gmail.com>
2024-10-11 11:50:32 -0500

    map

---
+++ DS3/map/src/MySet.java
@@ -0,0 +1,59 @@
+import java.util.Iterator;
+import java.util.ArrayList;
+public class MySet<T> implements SetInterface<T>, Iterable<T> {
+
+    private ArrayList<T> set = new ArrayList<T>();
+    Iterator<T> iterator = set.iterator();
+
+    public MySet(){
+        set = new ArrayList<T>();
+    }
+
+    @Override
+    public boolean add(T o){
+        if(set.contains(o)){
+            return false;
+        }
+        set.add(o);
+        return true;
+    }
+
+    @Override
+    public void clear(){
+        set.clear();
+    }
+
+    @Override
+    public boolean contains(T o){
+        return set.contains(o);
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return set.isEmpty();
+
+    }
+
+    @Override
+    public Iterator<T> iterator(){
+        return iterator;
+    }
+
+    @Override
+    public boolean remove(T o){
+        if(set.contains(o)){
+            set.remove(o);
+            return true;
+        }
+        return false;
+        
+    }
+    
+    @Override
+    public int size(){
+        return set.size();
+    }
+
+
+
+}
